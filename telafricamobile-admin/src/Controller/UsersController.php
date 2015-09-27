b1f44dece958052e645ab374f5a88a3870ca48e7
Tendai Mpita <malvern@integrat.co.za>
2015-09-27 21:51:23 +0200

    Updated the following files to make the registration work : still in progress

--- telafricamobile-admin/src/Controller/UsersController.php
+++ telafricamobile-admin/src/Controller/UsersController.php
@@ -6,33 +6,54 @@ use App\Controller\AppController;
 use Cake\Event\Event;
 
 class UsersController extends AppController{
-/**
+
 	public function beforeFilter(Event $event){
 		parent::beforeFilter($event);
-		$this->Auth->allow('register');
+		// Allow users to register and logout.
+		// You should not add the "login" action to allow list. Doing so would
+		// cause problems with normal functioning of AuthComponent.
+		$this->Auth->allow(['register', 'logout']);
 	}
-**/
-	public function index()	{
-		$this->set('users', $this->Users->find('all'));
+	
+	public function login(){
+		if ($this->request->is('post')) {
+			$user = $this->Auth->identify();
+			if ($user) {
+				$this->Auth->setUser($user);
+				return $this->redirect($this->Auth->redirectUrl());
+			}
+			$this->Flash->error(__('Invalid username or password, try again'));
+		}
 	}
+
+	 public function index(){
+        $this->set('users', $this->paginate($this->Users));
+        $this->set('_serialize', ['users']);
+    }
 
 	public function view($id){
 
 		$user = $this->Users->get($id);
 		$this->set(compact('user'));
 	}
+	
 	public function register(){ 
 
 		$user = $this->Users->newEntity();
 		if ($this->request->is('post')) {
+			//echo "<pre>";print_r($this->request->data);echo "</pre>";
 			$user = $this->Users->patchEntity($user, $this->request->data);
 			if ($this->Users->save($user)) {
 				$this->Flash->success(__('The user has been saved.'));
-				return $this->redirect(['action' => 'add']);
+				return $this->redirect(['action' => 'register']);
 			}
 			$this->Flash->error(__('Unable to add the user.'));
 		}
 		$this->set('user', $user);
+	}
+
+	public function logout(){
+		return $this->redirect($this->Auth->logout());
 	}
 }
 ?>
