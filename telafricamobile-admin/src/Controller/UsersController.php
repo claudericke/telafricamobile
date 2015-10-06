d38137ee373458d3eccff9ca73efbabe3d625309
Tendai Mpita <malvern@integrat.co.za>
2015-10-06 08:09:43 +0200

    Add the logout functionality

--- telafricamobile-admin/src/Controller/UsersController.php
+++ telafricamobile-admin/src/Controller/UsersController.php
@@ -15,7 +15,7 @@ class UsersController extends AppController{
 		// Allow users to register and logout.
 		// You should not add the "login" action to allow list. Doing so would
 		// cause problems with normal functioning of AuthComponent.
-		$this->Auth->allow(['register', 'login']);
+		$this->Auth->allow(['register', 'login', 'logout']);
 	}
 	
 	public function login(){
