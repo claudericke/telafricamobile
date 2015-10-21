7c2e548982b2bda40461c62538db1a22957c58d9
claudericke <claudericke@gmail.com>
2015-10-21 11:45:05 +0200

    Value Added Services

---
+++ vas.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+      <title>telafrica Mobile - Voice Solutions</title>
+      <?php include("includes/header.php"); ?>
+</head>
+
+<body>
+    <div class="loader">
+        <div class="bubblingG">
+            <img src="img/preloader.GIF" alt="..." />
+        </div>
+    </div>
+
+    <!-- Start Navigation -->
+    <nav class="overlay-nav sticky-nav nav-transparent">
+       <?php include("includes/navigation.php"); ?>
+    </nav>
+    <!-- Start Navigation -->
+
+
+    <header class="divider-background background-parallax">
+
+        <img alt="Header Background" class="background-image" src="img/header3.jpg">
+
+        <div class="row">
+            <div class="medium-12 medium-centered columns text-center">
+                <h4 class="text-white underline">telafrica Value Added Services</h4>
+            </div>
+        </div>
+
+    </header>
+
+    <section>
+        <!--begin text section-->
+
+
+
+        <div class="row">
+            <div class="medium-12 columns">
+                    <h6 class="page-title">VALUE ADDED SERVICES</h6>
+                    <p>telafrica Mobile offers many more services beyond Voice, Data and SMS and we classify them as  Value Added Services. telafrica conducts theses services as a member of WASPA.  The range of services we offer are as follows;</p>
+                <ul class="bulletList">
+                    <li>SMS Competitions</li>
+                    <li>SMS Voting</li>
+                    <li>SMS Lottery</li>
+                    <li>Ringtones</li>
+                    <li>Videos</li>
+                    <li>Games</li>
+                    <li>Wallpapers</li>
+                    <li>IVR Implementation</li>
+                </ul><br/>
+                <p>Please visit the link below for the WASPA code of conduct;</p>
+                <p><a href="https://waspa.org.za/coc/14-1/">https://waspa.org.za/coc/14-1/</a></p>
+            <p>Should you have any enquiry please send an email to <a href="mailto:info@telafricamobile.co.za">info@telafricamobile.co.za</a> or call our <strong>Customer Care Centre on 081 900 0000</strong> </p>
+            </div>
+        </div>
+    </section>
+    <?php include("includes/footer.php"); ?>
+
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/smooth-scroll.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/inview.js"></script>
+    <script src="js/skrollr.min.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/sweetalert.min.js"></script>
+    <script src="js/jquery.smoothwheel.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/verify.js"></script>
+
+    <script>
+        $(document).foundation();
+
+
+
+        //Smooth Scrollig on Page
+        $(document).ready(function() {
+            $("body").smoothWheel();
+
+        });
+
+
+    </script>
+</body>
+
+</html>
